cf1f0ae006b1f0f7d6497a4879e6f3ce3fffc7a1
Daniel20013 <126924076+Daniel20013@users.noreply.github.com>
2025-10-06 08:50:02 +0300

    Delete src directory

--- src/main/java/com/example/Presidential/elections/V1/Repository/CandidateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Presidential.elections.V1.Repository;
-
-import com.example.Presidential.elections.V1.Entity.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-    Candidate findByEmail(String email);
-}
