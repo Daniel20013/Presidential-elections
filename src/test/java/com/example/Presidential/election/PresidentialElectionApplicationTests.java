27aeb19b3c7a3dfd34c28de057eed8dfceb7f4cb
Daniel20013 <126924076+Daniel20013@users.noreply.github.com>
2025-10-06 08:47:27 +0300

    Add files via upload

---
+++ src/test/java/com/example/Presidential/election/PresidentialElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Presidential.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresidentialElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
